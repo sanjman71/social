9f105636e17359ad25bbd49bfc6abdb3b2c0fa1e
Sanjay <sanjay@jarna.com>
2010-09-05 23:27:45 -0500

    added google maps key for production

--- config/initializers/maps.rb
+++ config/initializers/maps.rb
@@ -1,1 +1,6 @@
-GOOGLE_MAPS_KEY = "ABQIAAAAomTSMjVMlOfQaldkZBqMBBQf8Rzy2ZcACVxYDKeiU5AgHyYWGBR1k1cjUQ1lJBbPUXEoFNRuqKCrgA"
+case Rails.env
+when 'development', 'test'
+  GOOGLE_MAPS_KEY = "ABQIAAAAomTSMjVMlOfQaldkZBqMBBQf8Rzy2ZcACVxYDKeiU5AgHyYWGBR1k1cjUQ1lJBbPUXEoFNRuqKCrgA"
+when 'production'
+  GOOGLE_MAPS_KEY = "ABQIAAAAomTSMjVMlOfQaldkZBqMBBRB5sHKHbBpNwpljJacvnA0UY36WxRxrGHm0Q6efneLmObbv1mf2zsWDw"
+end
