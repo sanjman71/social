513b50eb30cd21410fef7e7fad26e4a6d31ccace
Sanjay <sanjay@jarna.com>
2010-09-21 17:57:30 -0500

    updated local google maps api key

--- config/initializers/maps.rb
+++ config/initializers/maps.rb
@@ -1,6 +1,6 @@
 case Rails.env
 when 'development', 'test'
-  GOOGLE_MAPS_KEY = "ABQIAAAAomTSMjVMlOfQaldkZBqMBBT_3IJgYfW4oPtWVUONtg01ZmXmXBRZ7ADW9bvU4n_rpCGJKL4ao3jIFg"
+  GOOGLE_MAPS_KEY = "ABQIAAAAiuumaQ8zCrVmDaXTvnOnxRQSl1GINAMAsdLCp3TGJWdlrkoABBQ0WmpCkxE8nTeVlJLxz8rVm5FSqw"
 when 'production'
   GOOGLE_MAPS_KEY = "ABQIAAAAomTSMjVMlOfQaldkZBqMBBQCvIYwEwIoOiUyef86MtXNA8n6SxSIIcOVGTPPrLzSanErRbvLcnQy1g"
 end
