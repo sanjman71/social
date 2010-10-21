24e1c8d566bcf212097fe8b05eba78bd2cf8184f
Sanjay <sanjay@jarna.com>
2010-10-20 23:07:46 -0500

    added home#index dialog for link 'I plan to go here'

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -12,11 +12,18 @@ $(document).ready(function() {
 
   // initialize dialog
   $("#meet_user_dialog").dialog({modal: true, autoOpen: false, width: 350, height: 150, show: 'fadeIn(slow)'});
+  $("#plan_location_dialog").dialog({modal: true, autoOpen: false, width: 350, height: 150, show: 'fadeIn(slow)'});
 
-  $("a#meet_user").click(function() {
+  $("a#meet_user").live('click', function() {
     handle = $(this).parents('.match').attr('data-handle');
     $("#meet_user_dialog #handle").text("We'll connect you with " + handle);
     $("#meet_user_dialog").dialog('open');
     return false;
   })
+
+  $("a#plan_location").live('click', function() {
+    $("#plan_location_dialog").dialog('open');
+    return false;
+  })
+
 })
