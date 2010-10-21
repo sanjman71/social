bfc809f7762b6a0ecb1e3afd379cf5d58fdbb999
Sanjay <sanjay@jarna.com>
2010-10-21 17:15:56 -0500

    changed bundle install to use explicit install path

--- config/recipes/bundle.rb
+++ config/recipes/bundle.rb
@@ -2,15 +2,15 @@ namespace :bundle do
 
   desc "Install bundled gems"
   task :install, :roles => [:app, :db] do
-    run "bash -ic 'cd #{current_path}; bundle install'"
+    run "bash -ic 'cd #{current_path}; bundle install /usr/apps/social/shared/vendor/bundle'"
   end
  
   # after deploy task
-  desc "Update .bundle/config"
-  task :config, :roles => [:app, :db] do
-    puts "deploying .bundle/config"
-    run "mkdir -p #{current_release}/.bundle"
-    run "cp #{current_release}/config/templates/bundle.#{rails_env}.config #{current_release}/.bundle/config"
-  end
+  # desc "Update .bundle/config"
+  # task :config, :roles => [:app, :db] do
+  #   puts "deploying .bundle/config"
+  #   run "mkdir -p #{current_release}/.bundle"
+  #   run "cp #{current_release}/config/templates/bundle.#{rails_env}.config #{current_release}/.bundle/config"
+  # end
 
 end
