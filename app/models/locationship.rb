d0dd3624f0880b6f0b61d66ac9e4f20bdd3426a1
Sanjay <sanjay@jarna.com>
2010-11-06 10:54:38 -0500

    changed locationship to resolve planned checkins on a user checkin

--- app/models/locationship.rb
+++ app/models/locationship.rb
@@ -5,7 +5,9 @@ class Locationship < ActiveRecord::Base
   validates     :user_id, :presence => true
 
   after_create  :event_locationship_created
-  after_save    :touch_planned_at
+  after_save    :event_locationship_saved
+
+  attr_reader   :planned_checkin_resolution
 
   scope         :my_checkins, where(:my_checkins.gt => 0)
   scope         :planned_checkins, where(:planned_checkins.gt => 0)
@@ -26,6 +28,16 @@ class Locationship < ActiveRecord::Base
     locationship
   end
 
+  # after save filter
+  def event_locationship_saved
+    touch_planned_at
+    resolve_planned_checkins
+  end
+
+  def user_checkins
+    user.checkins.where(:location_id => location_id)
+  end
+
   def self.log(level, s, options={})
     CHECKINS_LOGGER.info("#{Time.now}: [#{level}] #{s}")
     if level == :error
@@ -35,10 +47,24 @@ class Locationship < ActiveRecord::Base
 
   protected
 
+  # update planned_at timestamp when user plans a checkin
   def touch_planned_at
     if changes[:planned_checkins] == [0,1]
       touch(:planned_at)
     end
   end
 
+  # resolve planned checkin when a user checks in to the location
+  def resolve_planned_checkins
+    if changes[:my_checkins] == [0,1] and planned_checkins == 1
+      # check timestamp
+      if planned_at > 7.days.ago
+        @planned_checkin_resolution = :completed
+      else
+        @planned_checkin_resolution = :toolate
+      end
+      # reset planned_checkins
+      decrement!(:planned_checkins)
+    end
+  end
 end
--- test/unit/locationship_test.rb
+++ test/unit/locationship_test.rb
@@ -33,4 +33,28 @@ class LocationshipTest < ActiveSupport::TestCase
     @locship    = @user1.locationships.create!(:location => @location1, :planned_checkins => 1)
     assert @locship.reload.planned_at
   end
+  
+  should "resolve as completed when a user checks in to a planned location within the time limit" do
+    @user1      = Factory.create(:user)
+    @location1  = Location.create(:name => "Location 1", :country => @us)
+    # user plans a checkin
+    @locship    = @user1.locationships.create!(:location => @location1, :planned_checkins => 1)
+    @locship.update_attribute(:planned_at, 3.days.ago)
+    # user checks in to the location
+    @locship.increment!(:my_checkins)
+    assert_equal :completed, @locship.planned_checkin_resolution
+    assert_equal 0, @locship.reload.planned_checkins
+  end
+
+  should "resolve as too-late when a user checks in to a planned location after the time limit" do
+    @user1      = Factory.create(:user)
+    @location1  = Location.create(:name => "Location 1", :country => @us)
+    # user plans a checkin
+    @locship    = @user1.locationships.create!(:location => @location1, :planned_checkins => 1)
+    @locship.update_attribute(:planned_at, 10.days.ago)
+    # user checks in to the location
+    @locship.increment!(:my_checkins)
+    assert_equal :toolate, @locship.planned_checkin_resolution
+    assert_equal 0, @locship.reload.planned_checkins
+  end
 end
