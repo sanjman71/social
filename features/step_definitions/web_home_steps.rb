9d134665e4ad778729faf991625b48c415238ed3
Sanjay <sanjay@jarna.com>
2010-11-23 14:31:18 -0600

    removed unused cucumber steps file

--- features/step_definitions/web_home_steps.rb
+++
@@ -1,6 +0,0 @@
-Then /^I should see user "([^"]*)" in stream "([^"]*)"$/ do |handle, stream|
-  # should have 'stream' marked as current stream
-  page.has_selector?("span.stream_name.current", :text => stream.titleize)
-  # should have user checkin in stream
-  assert page.has_selector?("div#checkin_user_location", :text => handle)
-end
