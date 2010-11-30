b162f8b70be913540dec088afcaa0dfc8fc0d876
Sanjay <sanjay@jarna.com>
2010-11-29 18:25:42 -0600

    created cucumber custom_env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -63,26 +63,3 @@ if defined?(ActiveRecord::Base)
   end
 end
 
-require 'cucumber/thinking_sphinx/external_world'
-Cucumber::ThinkingSphinx::ExternalWorld.new
-
-module LocalityWorld
-  def init_states_and_cities
-    # add default states, cities
-    ca = State.find_or_create_by_name(:name => 'California', :code => 'CA', :country => Country.us)
-    il = State.find_or_create_by_name(:name => 'Illinois', :code => 'IL', :country => Country.us)
-    ma = State.find_or_create_by_name(:name => 'Massachusetts', :code => 'MA', :country => Country.us)
-    ny = State.find_or_create_by_name(:name => 'New York', :code => 'NY', :country => Country.us)
-  
-    chicago = City.find_or_create_by_name(:name => 'Chicago', :state => il, :lat => 41.8781136, :lng => -87.6297982)
-  end
-end
-
-World(LocalityWorld)
-
-Before do
-  ThinkingSphinx::Test.index
-  Badges::Init.add_roles_and_privileges
-  init_states_and_cities
-end
-
