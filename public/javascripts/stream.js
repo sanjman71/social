20790cbafd242c021f5627157f4df4570cd7e615
Sanjay <sanjay@jarna.com>
2010-12-11 23:06:49 -0600

    added home.js

--- public/javascripts/stream.js
+++ public/javascripts/stream.js
@@ -104,21 +104,8 @@ $.fn.init_stream_timer = function() {
   }
 }
 
-$.fn.init_tooltips = function() {
-  $("a#map_wtf").tooltip({effect: 'fade', predelay: 100, fadeOutSpeed: 100, position: "bottom right",
-                          offset: [-70, -250]});
-  $("a#map_wtf").click(function() { return false; })
-  $("a#suggestions_wtf").tooltip({effect: 'fade', predelay: 100, fadeOutSpeed: 100, position: "top right",
-                                  offset: [0, 0]});
-  $("a#suggestions_wtf").click(function() { return false; })
-  $("a#outlately_wtf").tooltip({effect: 'fade', predelay: 100, fadeOutSpeed: 100, position: "top right",
-                                offset: [0, 0]});
-  $("a#outlately_wtf").click(function() { return false; })
-}
-
 $(document).ready(function() {
   $(document).init_stream_map();
   $(document).init_stream_objects();
   $(document).init_stream_timer();
-  $(document).init_tooltips();
 })
