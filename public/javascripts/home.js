cc4f2046b83690001a557e0731b7b9d64619a9b5
Sanjay <sanjay@jarna.com>
2010-12-17 10:11:10 -0600

    added exception handling

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,12 +1,15 @@
 $.fn.init_tooltips = function() {
-  $("a#map_wtf").tooltip({effect: 'fade', predelay: 100, fadeOutSpeed: 100, position: "bottom right",
-                          offset: [-70, -250]});
-  $("a#map_wtf").click(function() { return false; })
-  $("a#suggestions_wtf").tooltip({effect: 'fade', predelay: 100, fadeOutSpeed: 100, position: "top right",
+  try {
+    $("a#map_wtf").tooltip({effect: 'fade', predelay: 100, fadeOutSpeed: 100, position: "bottom right",
+                            offset: [-70, -250]});
+    $("a#suggestions_wtf").tooltip({effect: 'fade', predelay: 100, fadeOutSpeed: 100, position: "top right",
+                                    offset: [0, 0]});
+    $("a#outlately_wtf").tooltip({effect: 'fade', predelay: 100, fadeOutSpeed: 100, position: "top right",
                                   offset: [0, 0]});
+  } catch(e) { }
+
+  $("a#map_wtf").click(function() { return false; })
   $("a#suggestions_wtf").click(function() { return false; })
-  $("a#outlately_wtf").tooltip({effect: 'fade', predelay: 100, fadeOutSpeed: 100, position: "top right",
-                                offset: [0, 0]});
   $("a#outlately_wtf").click(function() { return false; })
 }
 
