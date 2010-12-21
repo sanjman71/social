fe49dfed2e253fc28891576c7bb59fba31473f6e
Sanjay <sanjay@jarna.com>
2010-12-21 15:43:00 -0600

    changed secret as required by new version of devise

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Social::Application.config.secret_token = 'ec6b30244f64c77281dab3750816fc0e4d32dffe62adad2a832fddc56137ee38fced64da23536c4c7f49e48cdfe1810aef78369af62c596c875c89d4f070dd44'
+Social::Application.config.secret_token = 'ec6b30244f64c77281dab3750816fc0e4d32dffe62adad2a832fddc56137ee38fced64da23536c4c7f49e48cdfe1810aef78369af62c596c875c89d4f070dd47'
