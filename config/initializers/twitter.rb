be21ee85774a7eba726a61d7ce1e6690dbd2face
Sanjay <sanjay@jarna.com>
2011-01-02 00:53:10 -0600

    added twitter controller for testing twitter gem

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "yV8cZBSAsuAXRMNsHTILfQ"
+  config.consumer_secret = "tihx2ZjPLrRIKgoXA15IwcCA33e2K2DeuyyVOjgdE"
+  config.oauth_token = ''
+  config.oauth_token_secret = ''
+end
