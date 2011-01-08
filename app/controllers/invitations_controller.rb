1b81a767eead00969938a61c2369114377c0cc9f
Sanjay <sanjay@jarna.com>
2011-01-08 03:53:10 -0600

    changed invite page to also show friends who are members

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -9,9 +9,10 @@ class InvitationsController < ApplicationController
       @to = User.find_by_id($1)
     end
     @invitation = current_user.invitations.new
-    # find non-member friends with checkins
-    @friends    = (current_user.friends + current_user.inverse_friends)
-    @friends    = @friends.select{ |u| !u.member and u.checkins_count > 0 }.sort_by{ |u| -1 * u.checkins_count }
+    # find friends with checkins
+    @friends    = (current_user.friends + current_user.inverse_friends).select{ |u| u.checkins_count > 0 }.sort_by{ |u| -1 * u.checkins_count }
+    # partition into friends that are members and not
+    @fmembers, @finvitees = @friends.partition{ |f| f.member? }
   end
 
   # POST /invite
