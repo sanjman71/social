55bde58f23108f772c82c1732181d9db5da40988
Sanjay <sanjay@jarna.com>
2011-01-18 11:13:19 -0600

    remove checkin:stats from daily cron

--- config/schedule.outlately.rb
+++ config/schedule.outlately.rb
@@ -50,8 +50,3 @@ every 1.hour do
   # expire planned checkins
   command "cd /usr/apps/outlately/current && thor checkins:expire_planned_checkins >> /usr/apps/outlately/shared/log/checkin_todo_expired.log"
 end
-
-every 1.day, :at => '7:00 am' do
-  # send reports
-  command "cd /usr/apps/outlately/current && thor checkins:stats --sendto='sanjay@jarna.com,marchick@gmail.com' >> /usr/apps/outlately/shared/log/checkin_stats.log"
-end
