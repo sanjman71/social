48e14b8cb9bf63395dafe4f7632a1ffaba76d7cb
Sanjay <sanjay@jarna.com>
2011-02-06 12:07:07 -0600

    updated user handle deploy script

--- script/deploy/reset_user_handles.rb
+++ script/deploy/reset_user_handles.rb
@@ -2,9 +2,15 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '../..', 'config', 'environment'))
 
 # find all non-members, and reset user handles
+updated = 0
 User.non_member.each do |user|
-  user.handle = User.handle_from_full_name(user.handle)
-  user.save
+  new_handle = User.handle_from_full_name(user.handle)
+  if new_handle != user.handle
+    # change handle
+    user.handle = new_handle
+    user.save
+    updated += 1
+  end
 end
 
-puts "#{Time.now}: updated user handles"
+puts "#{Time.now}: updated #{updated} user handles"
