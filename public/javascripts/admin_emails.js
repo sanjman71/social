a41927af00d0ef4626e4a998d378fd5c3d36d876
Sanjay <sanjay@jarna.com>
2011-03-23 19:40:38 -0500

    updated admin js emails chart

--- public/javascripts/admin_emails.js
+++ public/javascripts/admin_emails.js
@@ -8,14 +8,11 @@ $(document).ready(function() {
     },
     series:
       [{name: 'Badge Added', pointInterval: 24 * 3600 * 1000, pointStart: dstart, data: email_hash['badge_added']},
-       {name: 'Daily Checkins', pointInterval: 24 * 3600 * 1000, pointStart: dstart, data: email_hash['daily_checkins']},
+       {name: 'Friend Realtime Checkins', pointInterval: 24 * 3600 * 1000, pointStart: dstart, data: email_hash['friend_realtime_checkin']},
        {name: 'Imported Checkins', pointInterval: 24 * 3600 * 1000, pointStart: dstart, data: email_hash['imported_checkin']},
        {name: 'Invite', pointInterval: 24 * 3600 * 1000, pointStart: dstart, data: email_hash['invite']},
        {name: 'Message', pointInterval: 24 * 3600 * 1000, pointStart: dstart, data: email_hash['message']},
-       {name: 'Realtime Checkins', pointInterval: 24 * 3600 * 1000, pointStart: dstart, data: email_hash['realtime_checkins']},
        {name: 'Share Drink', pointInterval: 24 * 3600 * 1000, pointStart: dstart, data: email_hash['share_drink']},
-       {name: 'Todo Added', pointInterval: 24 * 3600 * 1000, pointStart: dstart, data: email_hash['todo_added']},
-       {name: 'Todo Expired', pointInterval: 24 * 3600 * 1000, pointStart: dstart, data: email_hash['todo_expired']},
       ]
   });
 })
