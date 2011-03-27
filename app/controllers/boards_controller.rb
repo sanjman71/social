6a323589e7c15c085113a3a066c9c9f7e062af87
Sanjay <sanjay@jarna.com>
2011-03-26 23:26:57 -0500

    added board#show and added links from home page

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,6 +5,12 @@ class BoardsController < ApplicationController
   def index
     # find user's most recent chalkboard
     @chalkboard = Wall.find_by_member(current_user)
+    redirect_to(board_path(@chalkboard.id))
   end
-  
+
+  # GET /boards/1
+  def show
+    @chalkboard = Wall.find(params[:id])
+  end
+
 end
